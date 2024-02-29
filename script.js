834290d0ec8670c26870c9eccdf4a3014b4aa3e0
Lvga21 <132945697+lVega21@users.noreply.github.com>
2024-02-29 11:24:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-
-let vida = 6;
+let vida=6;
 let palabra = "APPLE";
 const GRID = document.getElementById('cuadros');
 const vidaPantalla = document.getElementById('vida_Pantalla');
+reiniciar ();
 
 
 function letra(letra) {
-    console.log("Letra presionada:", letra);
-    var text = document.getElementById ('texto');
-    text.value += letra;
+        console.log("Letra presionada:", letra);
+        var text = document.getElementById ('texto');
+        text.value += letra;
 }
+
 function borrar() {
-    var text = document.getElementById ('texto');
-    text.value =  text.value.slice(0, -1);
+        var text = document.getElementById ('texto');
+        text.value =  text.value.slice(0, -1);
 }
+
 
 
 
@@ -23,94 +25,89 @@ const intp = document.getElementById ("texto");
 
 
 function leerIntento(){
-let intento = document.getElementById ("texto")
-intento = intento.value;
-intento = intento.toUpperCase();
-return intento;
+        let intento = document.getElementById ("texto")
+        intento = intento.value;
+        intento = intento.toUpperCase();
+        return intento;
 
 }
 
 
 
 function intentar(){
+        const intenTo = leerIntento()
+        vida --;
+        vidaPantalla.innerText = `${vida}`;
+            if (intenTo === palabra){
+                terminar("<span class='verde'>GANASTE!!!</span>");
+                setTimeout(reiniciar,1000);
+                return
+                }
 
-    const intenTo = leerIntento()
-
-    vida --;
-    vidaPantalla.innerText = `${vida}`;
-    if (intenTo === palabra){
-        terminar("<span class='verde'>GANASTE!!!</span>");
-        reiniciar();
-        return
-        
-    }
-
-    if (vida === 0){
-        terminar("<span class='rojo'>PERDISTE!</span>");
-        reiniciar();
-        return
+            if (vida === 0){
+                terminar("<span class='rojo'>PERDISTE!</span>");
+                setTimeout(reiniciar,2000);
+                return
     
-    }
+                }
 
 
-    const ROW = document.createElement('div');
-    ROW.className = 'cuaDros';
+const ROW = document.createElement('div');
+ROW.className = 'cuaDros';
+
 for (let i in palabra){
     
-    const spam = document.createElement('span');
-    spam.className= 'letter';
+        const spam = document.createElement('span');
+        spam.className= 'letter';
 
-    if (intenTo [i] === palabra[i]){
-       spam.innerHTML = intenTo[i];
-       spam.classList.add ('green');}
+            if (intenTo [i] === palabra[i]){
+                spam.innerHTML = intenTo[i];
+                spam.classList.add ('green');}
 
-    else if (palabra.includes(intenTo[i])){
-        spam.innerHTML = intenTo[i];
-        spam.classList.add ('yellow');
-    }
-    else{
-        spam.innerHTML = intenTo[i];
-        spam.classList.add  ('grey');
-    }
-    ROW.appendChild(spam);
-    ROW.appendChild(document.createTextNode("  "));
+            else if (palabra.includes(intenTo[i])){
+                 spam.innerHTML = intenTo[i];
+                 spam.classList.add ('yellow');
+            }
+            else{
+                spam.innerHTML = intenTo[i] || ' ';
+                spam.classList.add  ('grey');
+            }
+
+ROW.appendChild(spam);
+ROW.appendChild(document.createTextNode("  "));
 
 }
 GRID.appendChild(ROW);
 
 }
+
+
 function terminar(mensaje){
 
-const resultado = document.getElementById("win");
-resultado.innerHTML = mensaje;
-
-
+        const resultado = document.getElementById("win");
+        resultado.innerHTML = mensaje;
 }
 
 
 
-
-
 function reiniciar (){
-vida=6;
-palabra= nuevaPalabra();
+        vida=6;
+        palabra= "APPLE";
 
 
-const resultado =document.getElementsById ("win");
-resultado.innerHTML = "";
+        const resultado =document.getElementById ("win");
+            resultado.innerHTML = "";
 
 
-const GRID = document.getElementById("cuadros");
-GRID.innerHTML = "";
+        const GRID = document.getElementById("cuadros");
+            GRID.innerHTML = "";
 
 
-const vidaPantalla = document.getElementById("vida_pantalla");
-vidaPantalla.innerText = vida;
+        const vidaPantalla = document.getElementById("vida_Pantalla");
+            vidaPantalla.innerHTML = vida;
 
-const intento = document.getElementById ("texto");
-intento.value = "";
-
-
-    
+        const intento = document.getElementById ("texto");
+            intento.value = "";
+ 
 }
 
